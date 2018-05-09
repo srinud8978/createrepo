c70122bcc7993eab96d4403557dc4a68b3f9f7b1
Cashgen1 <Cashgen1@Cashgen>
2018-05-09 15:23:16 +0530

    one line added

---
+++ src/company/Test.java
@@ -0,0 +1,11 @@
+package company;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hai how are u");
+System.out.println("what r u doing");
+	}
+
+}
