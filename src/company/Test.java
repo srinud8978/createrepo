99bcf400759c204a5fb1d02e0d0c6e9504fd4740
Cashgen1 <Cashgen1@Cashgen>
2018-05-09 15:28:06 +0530

    psvm removed

--- src/company/Test.java
+++ src/company/Test.java
@@ -4,10 +4,6 @@ public class Test {
 private int empid;
 private String empname;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hai how are u");
-System.out.println("what r u doing");
 	}
 
-}
+
